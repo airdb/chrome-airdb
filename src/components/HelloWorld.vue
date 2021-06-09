7341551ed5e05fbee77b63d0473920dbd70bdd48
deancn <dean@airdb.com>
2021-06-09 13:40:32 +0800

    x

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,63 +1,88 @@
 <template>
   <div>
     <p>{{ defaultText }}</p>
-    <div class="" @keydown="($event)" @keyup.ctrl="ctrlHandler">
-        <div class="">
-          <input type="text" id="textbox" value="" placeholder="search" autofocus>
-        </div>
+    <span @click="goHome" class="home"
+      ><i class="iconfont">Home &#xe601;</i></span
+    >
+    <div class="" @keydown="$event;" @keyup.ctrl="ctrlHandler">
+      <div class="">
+        <input
+          type="text"
+          id="textbox"
+          value=""
+          placeholder="search"
+          autofocus
+        />
+      </div>
     </div>
 
     <div v-for="(item, index) in items" :key="index">
-       <a href="#" @click="openOn(item.url)">{{ item.name }}</a>
+      <a href="#" @click="openOn(item.url)">{{ item.name }}</a>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  mounted () {
-    browser.runtime.sendMessage({})
+  name: "HelloWorld",
+  mounted() {
+    browser.runtime.sendMessage({});
     document.getElementById("textbox").focus();
   },
-  data () {
+  data() {
     return {
       items: [
-        {"name": "vue-chrome-extersion", "url": "https://github.com/airdb/chrome-airdb"},
-        {"name": "uic swagger", "url": "https://scf.baobeihuijia.com/test/uic/"},
-        {"name": "login", "url": "https://scf.baobeihuijia.com/test/uic/login"},
-        {"name": "airdb.dev", "url": "https://airdb.dev"},
-        {"name": "airdb.io", "url": "https://airdb.io"},
-      ]
-    }
+        {
+          name: "vue-chrome-extersion",
+          url: "https://github.com/airdb/chrome-airdb",
+        },
+        { name: "uic swagger", url: "https://scf.baobeihuijia.com/test/uic/" },
+        { name: "login", url: "https://scf.baobeihuijia.com/test/uic/login" },
+        { name: "airdb.dev", url: "https://airdb.dev" },
+        { name: "airdb.io", url: "https://airdb.io" },
+      ],
+    };
   },
   methods: {
     focusInput() {
-      console.log("bbb")
-      this.$refs.search.$el.focus
-      console.log("ccc")
+      console.log("bbb");
+      this.$refs.search.$el.focus;
+      console.log("ccc");
+    },
+    goHome() {
+      chrome.tabs.create({
+        url: "https://noah.airdb.io",
+        selected: true,
+      });
     },
     openOn(url) {
-      console.log("xxx")
-      chrome.tabs.create( {
-	url: url
-      })
+      console.log("xxx");
+      chrome.tabs.create({
+        url: url,
+      });
     },
     openOnNewTab(newTab) {
-      console.log(newTab)
-      chrome.tabs.create(newTab)
-    }
+      console.log(newTab);
+      chrome.tabs.create(newTab);
+    },
   },
   computed: {
-    defaultText () {
-      return browser.i18n.getMessage('extName')
-    }
-  }
-}
+    defaultText() {
+      return browser.i18n.getMessage("extName") +'.' +browser.i18n.getDes();
+    },
+  },
+};
 </script>
 
 <style scoped>
 p {
   font-size: 20px;
+}
+
+.home {
+  width: 30px;
+  height: 30px;
+  font-size: 20px;
+  cursor: pointer;
 }
 </style>
